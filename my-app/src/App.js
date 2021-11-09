58bc0706b6f36e6e8fe2bb860659c9003fea56c7
Alexey <aleksejpleh@gmail.com>
2021-11-10 01:22:12 +0200

    Work with jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {MyComponents} from "./components/MyComponentClass";
+// import MyComponents from "./components/MyComponentFunction";
+// import Users from "./components/Users";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      // <MyComponents User={Users}/>
+      <MyComponents />
   );
 }
 
